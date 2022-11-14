ae2824ae0b5524357ecf2ecb6dfd55e821b5ae94
Mohammad Mokhtari <mohammadmokhtari917@gmail.com>
2022-11-14 01:26:23 -0800

    Strat Project Navigation Menu

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Menu from '../src/components/Menu/navigationMenu';
+
+function App() {
+  return (
+    <React.Fragment> 
+        <p>App</p>
+        <Menu/>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Menu/navigationMenu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function navigationMenu() {
+    return (
+    <React.Fragment> 
+        <p>navigationMenu</p>
+        <ul>
+            <li>GENERAL</li>
+            <li>PLANT</li>
+            <li>USER</li>
+        </ul>
+    </React.Fragment>
+    )
+}
+
+export default navigationMenu
